21448867007b8e1aa25933cfef863ba681608f0f
asgerbirk <asger.birk@gmail.com>
2023-04-20 15:57:28 +0200

    moved button so they are on the same line

--- myportfolio/src/components/Projects.jsx
+++ myportfolio/src/components/Projects.jsx
@@ -31,29 +31,29 @@ export const Projects = () => {
 
     ]
 
-return(
-    <div name="projects"  className="h-screen bg-gradient-to-b from-zinc-700 to-gray-800 text-white md:h-screen">
-        <div>
-            <div className="pb-12 text-center">
-                <h3 className="text-4xl font-bold mb-4 ">Here are some of my school projects</h3>
-            </div>
-            <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
+    return(
+        <div name="projects"  className="h-screen bg-gradient-to-b from-zinc-700 to-gray-800 text-white md:h-screen">
+            <div>
+                <div className="pb-12 text-center">
+                    <h3 className="text-4xl font-bold mb-4 ">Here are some of my school projects</h3>
+                </div>
+                <div className="grid grid-cols-1 md:grid-cols-3 gap-6">
 
-            {projects.map(({id, src, link, title, description}) => (
-                    <div key={id} className="bg-gray-900 rounded-lg overflow-hidden">
-                        <img src={src} alt="exam" className="w-full h-full max-h-64 object-cover object-center aspect-w-1 aspect-h-1"/>
-                        <div className="p-4">
-                            <h4 className="text-xl font-semibold mb-2">{title}</h4>
-                            <p className="text-gray-400">{description}</p>
-                            <div className="mt-4">
-                                <a href={link}  target="_blank" rel="noopener noreferrer" className="inline-block bg-zinc-500 hover:bg-zinc-600 text-white px-4 py-2 rounded">Code</a>
+                    {projects.map(({id, src, link, title, description}) => (
+                        <div key={id} className="bg-gray-900 rounded-lg overflow-hidden flex flex-col">
+                            <img src={src} alt="exam" className="w-full h-full max-h-64 object-cover object-center aspect-w-1 aspect-h-1"/>
+                            <div className="p-4 h-40 flex-grow">
+                                <h4 className="text-xl font-semi-bold mb-2">{title}</h4>
+                                <p className="text-gray-400">{description}</p>
+                            </div>
+                            <div className="p-4 flex justify-end items-end">
+                                <a href={link} target="_blank" rel="noopener noreferrer" className="bg-zinc-500 hover:bg-zinc-600 text-white px-4 py-2 rounded">Code</a>
                             </div>
                         </div>
-                    </div>
-            )) }
-            </div>
+                    )) }
+                </div>
 
+            </div>
         </div>
-    </div>
-)
+    )
 }
