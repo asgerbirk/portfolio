a18c37905deca5c5e891cb3bfcfa456d93be3e6d
asgerbirk <asger.birk@gmail.com>
2023-11-10 10:34:29 +0100

    home

--- myportfolio/src/components/Home.jsx
+++ myportfolio/src/components/Home.jsx
@@ -13,7 +13,7 @@ return(
                     Studying Computer Science at KEA
                 </h3>
                 <p className="max-w-md mx-auto mt-2">
-                    I am 24 years old, living in Nørrebro in Copenhagen and I'm searching for a 10-week unpaid internship. In my spare time I like to hang out with my friends, family and girlfriend.
+                    I am 25 years old, living in Nørrebro in Copenhagen and I'm searching for a student-job. I just finished my internship in TopTutors where i worked as an full-stack developer. In my spare time I like to hang out with my friends, family and girlfriend.
                     I am a competitive person and I enjoy all kind of sports and especially football. At the moment I play a lot of padel tennis and go to the gym.
                     In addition i enjoy playing computer games and chess.
                 </p>
