f756b54f6ed97781cd5e73ae63f3229d859a3434
asgerbirk <asger.birk@gmail.com>
2024-02-09 12:39:38 +0100

    contact update

--- myportfolio/src/components/Contact.jsx
+++ myportfolio/src/components/Contact.jsx
@@ -10,8 +10,8 @@ export const Contact = () => {
     };
 
     return (
-        <div name="contact" className="min-h-screen bg-white text-gray-800 flex flex-col justify-center items-center pt-12 pb-8">
-            <div className="text-center mb-8">
+        <div name="contact" className="min-h-screen bg-gradient-to-b from-zinc-700 to-gray-800 text-white flex flex-col justify-center items-center pt-12 pb-8">
+            <div className="text-center mb-4">
                 <h1 className="text-4xl font-bold">Contact</h1>
                 <p className="text-3xl mt-4">Asger Birk</p>
                 <p
@@ -24,16 +24,17 @@ export const Contact = () => {
                 <p className="text-3xl">+45 29892575</p>
             </div>
             <div className="flex justify-center gap-10">
-                <a href="https://github.com/asgerbirk" target="_blank" rel="noopener noreferrer" className="text-gray-800 text-4xl">
+                <a href="https://github.com/asgerbirk" target="_blank" rel="noopener noreferrer" className="text-4xl">
                     <FontAwesomeIcon icon={faGithub} />
                 </a>
-                <a href="https://www.linkedin.com/in/asger-birk-96741a234/" target="_blank" rel="noopener noreferrer" className="text-gray-800 text-4xl">
+                <a href="https://www.linkedin.com/in/asger-birk-96741a234/" target="_blank" rel="noopener noreferrer" className="text-4xl">
                     <FontAwesomeIcon icon={faLinkedinIn} />
                 </a>
             </div>
         </div>
     );
 };
+
 
 
 
