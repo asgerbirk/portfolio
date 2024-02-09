149debad683218ce118a29d419e22be411566bd6
asgerbirk <asger.birk@gmail.com>
2024-02-09 11:38:51 +0100

    changed link

--- myportfolio/src/components/Contact.jsx
+++ myportfolio/src/components/Contact.jsx
@@ -23,36 +23,38 @@ export const Contact = () => {
         }
     ]
 
-    return<div name="contact" className="h-screen bg-gradient-to-b from-zinc-700 to-gray-800 text-white md:h-screen">
-        <div className="text-center pb-12">
-            <h1 className="text-4xl font-bold mb-4">Contact</h1>
-        </div>
-        <div className="text-white text-center text-3xl mt-4">
-            <p className="mb-8">Asger Birk</p>
-
-            <p
-                title="Copy Email"
-                onClick={handleCopyEmail}
-                className="mb-8 cursor-pointer hover:underline"
-            >
-                Asger.birk@gmail.com</p>
-
-            <p className="mb-8">+45 29892575</p>
-        </div>
-
-        <div className="grid grid-cols-1 md:grid-cols-3 gap-16 ">
-            {contact.map(({id, src, link}) => (
-                <div key={id} className="rounded-lg overflow-hidden" style={{height: "400px"}}>
-                    <a href={link} target="_blank" rel="noopener noreferrer">
-                        <img
-                            src={src}
-                            alt="contact"
-                            className="w-full h-full object-cover"
-                        />
-                    </a>
+    return (
+        <div name="contact" className="h-screen bg-gradient-to-b from-zinc-700 to-gray-800 text-white md:h-screen">
+            <div className="text-center pb-12">
+                <h1 className="text-4xl font-bold mb-4">Contact</h1>
+            </div>
+            <div className="text-white text-center text-3xl mt-4">
+                <p className="mb-8">Asger Birk</p>
+                <p
+                    title="Copy Email"
+                    onClick={handleCopyEmail}
+                    className="mb-8 cursor-pointer hover:underline"
+                >
+                    Asger.birk@gmail.com
+                </p>
+                <p className="mb-8">+45 29892575</p>
+            </div>
+            <div className="flex justify-center items-center h-full">
+                <div className="grid grid-cols-1 md:grid-cols-3 gap-16 items-center justify-items-center">
+                    {contact.map(({id, src, link}) => (
+                        <div key={id} className="rounded-lg overflow-hidden" style={{ maxWidth: "250px", maxHeight: "250px" }}>
+                            <a href={link} target="_blank" rel="noopener noreferrer">
+                                <img
+                                    src={src}
+                                    alt="contact"
+                                    className="object-cover object-center w-full h-full"
+                                />
+                            </a>
+                        </div>
+                    ))}
                 </div>
-            ))}
-
+            </div>
         </div>
-    </div>
+    );
+
 }
